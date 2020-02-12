13c1615b3b802bf713dd11f07d68cf5bb306f1f5
kabascolby <kabascolby@gmail.com>
2020-02-11 18:44:13 -0800

    creating the network script

--- dependancy.sh
+++ dependancy.sh
@@ -15,11 +15,13 @@ apt install dnsutils && \
 #install network utils
 apt install net-tools && \
 #saving the default Gateway address
-GW=$(ip route show | grep default | awk '{ print $3}') && \
+GW=$(sudo ip route show | grep default | awk '{ print $3}') && \
+NI=$(sudo ip route show | grep –m1 default | awk '{print $5}') && \ 
 #updating the network interface address
-sudo echo "auto enp0s3 \
-  iface enp0s3 inet static\
+sudo echo "auto $NI \
+  iface $NI inet static\
   address 192.168.1.2\
   netmask 255.255.255.252\
   gateway $GW\
-  dns-nameservers 8.8.8.8 4.4.2.2" > /etc/network/interfaces 
+  dns-nameservers 8.8.8.8 4.4.2.2" > /etc/network/interfaces && \
+ifup $NI
---
+++ network.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#saving the default Gateway address
+GW=$(sudo ip route show | grep default | awk '{ print $3}') && \
+NI=$(sudo ip route show | grep –m1 default | awk '{print $5}') && \ 
+#updating the network interface address
+sudo echo "# The primary network interface\n
+auto $NI \n
+iface $NI inet static\n
+address 192.168.1.2\n
+netmask 255.255.255.252\n
+gateway $GW\n" >> /etc/network/interfaces && \
