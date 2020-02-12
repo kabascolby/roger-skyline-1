613616f3f7773d95a4821ed256912f4a47c33315
kabascolby <kabascolby@gmail.com>
2020-02-12 01:46:53 -0800

    add line to stop the service

--- network.sh
+++ network.sh
@@ -2,12 +2,18 @@
 
 #saving the default Gateway address
 GW=$(sudo ip route show | grep default | awk '{ print $3}') && \
-NI=$(sudo arp –n | grep ether | awk '{print $5}') && \ 
+NI=$(sudo arp –n | grep ether | awk '{print $5}') && \
+remove all the address 
+Sudo ip address flush $NI && \
+#stoping and disable the network daemon the services
+sudo systemctl stop NetworkManager.service && \
+sudo systemctl disable NetworkManager.service  && \
 #updating the network interface address
 sudo echo "# The primary network interface
 auto $NI
 iface $NI inet static
 address 192.168.1.2
 netmask 255.255.255.252
-gateway $GW" >> /etc/network/interfaces && \
-sudo systemctl restart NetworkManager.service
+gateway $GW
+#End" >> /etc/network/interfaces && \
+sudo service networking restart
