10223a6a562328d1635996c981deb7c7f4ea6170
kabascolby <kabascolby@gmail.com>
2020-02-11 19:55:43 -0800

    removing new lineswq

--- network.sh
+++ network.sh
@@ -4,10 +4,10 @@
 GW=$(sudo ip route show | grep default | awk '{ print $3}') && \
 NI=$(sudo arp –n | grep ether | awk '{print $5}') && \ 
 #updating the network interface address
-sudo echo "# The primary network interface\n
-auto $NI \n
-iface $NI inet static\n
-address 192.168.1.2\n
-netmask 255.255.255.252\n
-gateway $GW\n" >> /etc/network/interfaces && \
-sudo service networking restart
+sudo echo "# The primary network interface
+auto $NI
+iface $NI inet static
+address 192.168.1.2
+netmask 255.255.255.252
+gateway $GW" >> /etc/network/interfaces && \
+sudo systemctl restart NetworkManager.service
